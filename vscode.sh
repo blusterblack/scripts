e5ca3b3167ba353d2deafeea98385af6eea2f834
Lê Thanh Bình <lethanhbinh010309@gmail.com>
2021-02-23 18:45:02 -0800

    Change vscode theme from synthwave to one dark pro

--- vscode.sh
+++ vscode.sh
@@ -2,7 +2,7 @@ code --install-extension ms-python.python
 code --install-extension eamodio.gitlens
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension TabNine.tabnine-vscode
-code --install-extension robbowen.synthwave-vscode
+code --install-extension zhuangtongfa.material-theme
 code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension gruntfuggly.todo-tree
 code --install-extension ms-toolsai.jupyter
