a21adeb347f9bec9c0e4b1714767924da36769b4
Thanh Binh Le <ltbinh3999@gmail.com>
2021-02-25 19:39:16 +0700

    Convert window bullshit CRLF to LF

--- bootstrap-manjaro-i3.sh
+++ bootstrap-manjaro-i3.sh
@@ -1,24 +1,18 @@
 #!bin/bash
 # Bootstrap a newly created manjaro i3 system
 
-sudo pacman-mirrors --fasttrack
 sudo pacman -Syyu
 sudo pacman -Syu rofi xclip fcitx fcitx-configtool fcitx-unikey pavucontrol pulseaudio okular firefox fish guake ttf-fira-code noto-fonts-cjk
 sudo pacman -Rs epdfview palemoon-bin
 
-pamac build visual-studio-code-bin
-code --install-extension ms-python.python
-code --install-extension eamodio.gitlens
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension TabNine.tabnine-vscode
-code --install-extension ms-python.python
-
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash ./Anaconda3-2020.11-Linux-x86_64.sh
 rm ./Anaconda3-2020.11-Linux-x86_64.sh
+fish
 set -U fish_user_paths ~/anaconda3/bin $fish_user_paths
 conda init fish
 
+sudo timedatectl set-timezone Asia/Ho_Chi_Minh
 sudo timedatectl set-ntp true
 
 git clone https://github.com/ltbinh3999/dotfiles.git
