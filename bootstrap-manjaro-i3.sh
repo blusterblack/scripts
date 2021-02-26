528f2190c2cbc5244c4b9c339ad0edade2bac5cf
Thanh Binh Le <ltbinh3999@gmail.com>
2021-02-26 12:09:03 +0700

    Add installing stretchly

--- bootstrap-manjaro-i3.sh
+++ bootstrap-manjaro-i3.sh
@@ -2,8 +2,12 @@
 # Bootstrap a newly created manjaro i3 system
 
 sudo pacman -Syyu
-sudo pacman -Syu rofi xclip fcitx fcitx-configtool fcitx-unikey pavucontrol pulseaudio okular firefox fish guake ttf-fira-code noto-fonts-cjk
+sudo pacman -Syu rofi xclip fcitx fcitx-configtool fcitx-unikey pavucontrol pulseaudio okular firefox fish guake ttf-fira-code noto-fonts-cjk nodejs npm
 sudo pacman -Rs epdfview palemoon-bin
+
+wget https://github.com/hovancik/stretchly/releases/download/v1.4.0/Stretchly-1.4.0.pacman
+sudo pacman -U ./Stretchly-1.4.0.pacman
+rm ./Stretchly-1.4.0.pacman
 
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash ./Anaconda3-2020.11-Linux-x86_64.sh
--- setup-dwa131-driver.sh
+++ setup-dwa131-driver.sh
@@ -2,7 +2,6 @@
 # This script will install the driver of DWA-131 wifi USB for ManjaroOS.
 # Arch based distro can use this too.
 
-uname –r # Kernel version
 sudo pacman -Syu linux-headers dkms # Choose header version corresponding to your kernel version. Need to reboot.
 cd ~/
 git clone https://github.com/Mange/rtl8192eu-linux-driver
@@ -12,4 +11,4 @@ sudo dkms install rtl8192eu/1.0
 echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
 echo -e "8192eu\n\nloop" | sudo tee /etc/modules
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-echo 'Set up is completed. Reboot and connect to a network'
+echo "Run this script again after reboot then reboot again"
