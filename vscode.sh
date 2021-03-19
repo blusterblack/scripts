afa28a468aa2eb293ebac64556da25f9b303f1d9
Lê Thanh Bình <lethanhbinh010309@gmail.com>
2021-03-19 07:27:06 +0700

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,7 +1,6 @@
 code --install-extension ms-python.python
 code --install-extension eamodio.gitlens
 code --install-extension vscode-icons-team.vscode-icons
-code --install-extension TabNine.tabnine-vscode
 code --install-extension zhuangtongfa.material-theme
 code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension gruntfuggly.todo-tree
@@ -9,3 +8,4 @@ code --install-extension ms-toolsai.jupyter
 code --install-extension xabikos.javascriptsnippets
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.vscode-pylance
