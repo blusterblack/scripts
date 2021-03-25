165739dc619982af98441f3ad5573cb100575d16
Thanh Binh Le <ltbinh3999@gmail.com>
2021-03-25 16:03:07 +0700

    Change to KDE

--- setup-dwa131-driver.sh
+++ setup-dwa131-driver.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This script will install the driver of DWA-131 wifi USB for ManjaroOS.
-# Arch based distro can use this too.
 
-sudo pacman -Syu linux-headers dkms # Choose header version corresponding to your kernel version. Need to reboot.
+sudo pacman -Syu linux-headers dkms #
 cd ~/
 git clone https://github.com/Mange/rtl8192eu-linux-driver
 cd ~/rtl8192eu-linux-driver
 sudo dkms add .
 sudo dkms install rtl8192eu/1.0
+rm -rf ~/rtl8192eu-linux-driver
 echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
 echo -e "8192eu\n\nloop" | sudo tee /etc/modules
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-echo "Run this script again after reboot then reboot again"
+echo "Setup finish. Restart to use wifi"
